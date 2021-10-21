ebc5742964a95347f64d76c171610165c6c968c6
sorcho <mariobrosscot@yahoo.it>
2021-10-21 20:13:55 +0200

    Second Exercise

---
+++ secondoEsercizio/main.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+
+#include <iostream>
+
+int main() {
+    int numero;
+
+    cout<<"Inserisci un numero: ";
+    cin>>numero;
+    cout<<"Il numero inserito "<<char(130)<<": "<<numero;
+
+    return 0;
+}
