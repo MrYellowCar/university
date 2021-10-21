e855ad39c39614098044dbc030ed2e10f6817e4d
sorcho <mariobrosscot@yahoo.it>
2021-10-21 21:25:04 +0200

    update exercise

---
+++ primoEsercizio/cmake-build-debug/CMakeFiles/primoEsercizio.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/primoEsercizio.dir/main.cpp.obj"
+  "libprimoEsercizio.dll.a"
+  "primoEsercizio.exe"
+  "primoEsercizio.exe.manifest"
+  "primoEsercizio.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/primoEsercizio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ secondoEsercizio/cmake-build-debug/CMakeFiles/secondoEsercizio.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/secondoEsercizio.dir/main.cpp.obj"
+  "libsecondoEsercizio.dll.a"
+  "secondoEsercizio.exe"
+  "secondoEsercizio.exe.manifest"
+  "secondoEsercizio.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/secondoEsercizio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
