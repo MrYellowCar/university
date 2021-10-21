e64bfbae7b3bb6c850cfada9933a6cbf4caa1418
sorcho <mariobrosscot@yahoo.it>
2021-10-21 20:13:01 +0200

    First Exercise

---
+++ primoEsercizio/main.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+
+#include <iostream>
+
+int main() {
+
+    cout<<"Ciao sono io: il tuo Personal Computer."<<endl;
+    cout<<"Che desidera imparare tante cose nuove."<<endl;
+    cout<<"Devi solo progettare cosa devo fare."<<endl;
+    cout<<"Inventando nuovi programmi."<<endl;
+    cout<<"Forza datti da fare. Ci divertiremo."<<endl;
+
+    return 0;
+}
