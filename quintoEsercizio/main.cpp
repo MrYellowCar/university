8652cc6133f9ddff6058e89a1eb81cccb3e98219
sorcho <mariobrosscot@yahoo.it>
2021-10-28 17:48:03 +0200

    Sixth Exercise

--- quintoEsercizio/main.cpp
+++ quintoEsercizio/main.cpp
@@ -1,8 +1,19 @@
+/*Dato un numero intero in input scrivere se è maggiore di 10*/
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
+    int a;
 
+    cout << "Inserire il numero da confrontare: ";
+    cin >> a;
+
+    if (a > 10){
+        cout << "Il numero " << char(130) << " maggiore di 10!" << endl;
+    } else {
+        cout << "Il numero non " << char(130) << " maggiore di 10!" << endl;
+    }
     return 0;
 }
