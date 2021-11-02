2f4dfca6525a8c2cb247869e2a8c54e5dd1d008a
sorcho <mariobrosscot@yahoo.it>
2021-11-02 15:49:33 +0100

    Eserizi Tutorato

---
+++ tutoratoPrimoEsercizio/cmake-build-debug/CMakeFiles/tutoratoPrimoEsercizio.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tutoratoPrimoEsercizio.dir/main.cpp.obj"
+  "libtutoratoPrimoEsercizio.dll.a"
+  "tutoratoPrimoEsercizio.exe"
+  "tutoratoPrimoEsercizio.exe.manifest"
+  "tutoratoPrimoEsercizio.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tutoratoPrimoEsercizio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tutoratoSecondoEsercizio/cmake-build-debug/CMakeFiles/tutoratoSecondoEsercizio.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tutoratoSecondoEsercizio.dir/main.cpp.obj"
+  "libtutoratoSecondoEsercizio.dll.a"
+  "tutoratoSecondoEsercizio.exe"
+  "tutoratoSecondoEsercizio.exe.manifest"
+  "tutoratoSecondoEsercizio.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tutoratoSecondoEsercizio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
