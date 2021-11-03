d3e946736817671a400be00b8e01572b3ede8914
sorcho <mariobrosscot@yahoo.it>
2021-11-03 13:41:08 +0100

    Settimo Esercizio

---
+++ settimoEsercizio/cmake-build-debug/CMakeFiles/settimoEsercizio.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/settimoEsercizio.dir/main.cpp.obj"
+  "libsettimoEsercizio.dll.a"
+  "settimoEsercizio.exe"
+  "settimoEsercizio.exe.manifest"
+  "settimoEsercizio.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/settimoEsercizio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
