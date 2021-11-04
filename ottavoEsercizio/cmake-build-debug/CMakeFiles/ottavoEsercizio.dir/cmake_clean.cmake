a87c76f969b43f1f19d47a89a76fee97cd338112
sorcho <mariobrosscot@yahoo.it>
2021-11-04 17:59:57 +0100

    ottavo esercizio

---
+++ ottavoEsercizio/cmake-build-debug/CMakeFiles/ottavoEsercizio.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ottavoEsercizio.dir/main.cpp.obj"
+  "libottavoEsercizio.dll.a"
+  "ottavoEsercizio.exe"
+  "ottavoEsercizio.exe.manifest"
+  "ottavoEsercizio.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ottavoEsercizio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
