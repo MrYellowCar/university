a39a2232af16ad123a386fbbf8aa5c35f8b48a20
sorcho <mariobrosscot@yahoo.it>
2021-11-10 13:20:10 +0100

    Decimo Esercizio

---
+++ decimoEsercizio/cmake-build-debug/CMakeFiles/decimoEsercizio.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/decimoEsercizio.dir/main.cpp.obj"
+  "decimoEsercizio.exe"
+  "decimoEsercizio.exe.manifest"
+  "decimoEsercizio.pdb"
+  "libdecimoEsercizio.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/decimoEsercizio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ nonoEsercizio/cmake-build-debug/CMakeFiles/nonoEsercizio.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nonoEsercizio.dir/main.cpp.obj"
+  "libnonoEsercizio.dll.a"
+  "nonoEsercizio.exe"
+  "nonoEsercizio.exe.manifest"
+  "nonoEsercizio.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nonoEsercizio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
